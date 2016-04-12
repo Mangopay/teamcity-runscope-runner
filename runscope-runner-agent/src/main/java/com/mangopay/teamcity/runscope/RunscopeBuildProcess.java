ffcefa4ee359208e04d7e544606d776771f5f8bf
Bertrand Lemasle <bertrand.lemasle@leetchi.com>
2016-04-12 16:05:01 +0200

    Moved log message to RunscopeTestSet

--- runscope-runner-agent/src/main/java/com/mangopay/teamcity/runscope/RunscopeBuildProcess.java
+++ runscope-runner-agent/src/main/java/com/mangopay/teamcity/runscope/RunscopeBuildProcess.java
@@ -26,7 +26,6 @@ class RunscopeBuildProcess extends FutureBasedBuildProcess {
     @Override
     public BuildFinishedStatus call() throws Exception {
         final RunscopeTestSet runscopeTestSet = new RunscopeTestSet(token, bucket, tests, environment, logger);
-        logRunStart();
         runscopeTestSet.run();
 
         return BuildFinishedStatus.FINISHED_SUCCESS;
